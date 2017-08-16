a5c708570f16fc50a8fdbab83485d8b7f138f197
Rodger Jordas <mds3dstn71@protonmail.com>
2017-08-16 11:43:51 +0800

    Add postcss configuration file

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  map: false,
+  plugins: [
+    require('postcss-import'),
+    require('postcss-url'),
+    require('postcss-cssnext'),
+    // require('cssnano'),
+    require('postcss-reporter')
+  ],
+};
