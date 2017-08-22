eb6e954e215d2d01315e7bbb9a88ea6a19e05e5a
Rodger Jordas <mds3dstn71@protonmail.com>
2017-08-22 17:18:17 +0800

    Add cssnext components
    Not all cssnext components are used by PostCSS so these change includes
    only those that are needed.

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,35 @@
+const atImport = require('postcss-import');
+const apply = require('postcss-apply');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const customProperties = require('postcss-custom-properties');
+const customMedia = require('postcss-custom-media');
+const fontFamilySystemUi = require('postcss-font-family-system-ui');
+const mediaMinmax = require('postcss-media-minmax');
+const nesting = require('postcss-nesting');
+const reporter = require('postcss-reporter');
+const url = require('postcss-url');
+
 module.exports = {
   map: false,
   plugins: [
-    require('postcss-import'),
-    require('postcss-url'),
-    require('postcss-cssnext'),
-    require('cssnano'),
-    require('postcss-reporter')
+    atImport,
+    url,
+    autoprefixer({
+      remove: false,
+      grid: true,
+    }),
+    customProperties,
+    apply,
+    customMedia,
+    mediaMinmax,
+    nesting,
+    fontFamilySystemUi,
+    cssnano({
+      discardComments: {
+        removeAll: true,
+      },
+    }),
+    reporter,
   ],
 };
