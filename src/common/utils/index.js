f9c9b91cbdbe7aac3b4eaab54b43a3dc3f977bc5
Rodger Jordas <rmjordas@gmail.com>
2018-12-02 14:04:42 +0800

    Add props utility functions export

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -1,3 +1,4 @@
 export { default as numberUtils } from './number-utils';
 export { default as percentUtils } from './percent-utils';
+export { default as propsUtils } from './props-utils';
 export { default as stringUtils } from './string-utils';
