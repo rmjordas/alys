714bdabf5e592c88e7ec6e25e66c2ddbe924cf47
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 18:18:59 +0800

    Add TrashIcon component

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,4 @@ export { default as PreviewIcon } from './PreviewIcon';
 export { default as ProgressBar } from './ProgressBar';
 export { default as RemoveMentorIcon } from './RemoveMentorIcon';
 export { default as SvgIcon } from './SvgIcon';
+export { default as TrashIcon } from './TrashIcon';
---
+++ src/examples/TrashIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import TrashIcon from 'alys/TrashIcon';
+
+/** Basic trash icon */
+export default function ExampleBasic() {
+  return <TrashIcon />;
+}
