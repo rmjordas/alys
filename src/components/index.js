b0884f80968cd4d8f5a52d373a07732a529d70ee
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 22:25:31 +0800

    Add LaptopIcon component

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,7 @@ export { default as ComputerIcon } from './ComputerIcon';
 export { default as ConfirmIcon } from './ConfirmIcon';
 export { default as EmailIcon } from './EmailIcon';
 export { default as HomeIcon } from './HomeIcon';
+export { default as LaptopIcon } from './LaptopIcon';
 export { default as LikeIcon } from './LikeIcon';
 export { default as LiveIcon } from './LiveIcon';
 export { default as LocationIcon } from './LocationIcon';
---
+++ src/examples/LaptopIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import LaptopIcon from 'alys/LaptopIcon';
+
+/** Basic laptop icon */
+export default function ExampleBasic() {
+  return <LaptopIcon />;
+}
