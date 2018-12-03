cbd4efb6f4790709a9c11f6b68b26834961044b5
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 17:53:46 +0800

    Add HomeIcon component

---
+++ src/components/HomeIcon/HomeIcon.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SvgIcon from '../SvgIcon';
+
+export default function HomeIcon({ dark, ...svgProps }) {
+  const commonProps = {
+    fill: 'none',
+    stroke: dark ? '#fff' : '#4C5264',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    strokeMiterlimit: 10,
+  };
+
+  return (
+    <SvgIcon {...svgProps}>
+      <g id="home-icon" transform="translate(0 0)">
+        <path {...commonProps} id="path" d="M17.5,8.1h2.4L10.2.5.5,8.1h2" />
+        <path {...commonProps} id="path-2" data-name="path" d="M17.5,7.1" />
+        <path {...commonProps} id="path-3" data-name="path" d="M2.5,8.1" />
+        <path
+          {...commonProps}
+          id="path-4"
+          data-name="path"
+          d="M2.5,8.1v11h6v-6h3v6h6V8.1"
+        />
+      </g>
+    </SvgIcon>
+  );
+}
+
+HomeIcon.defaultProps = { dark: false };
+
+HomeIcon.propTypes = {
+  /** Set to `true` to make icon viewable in dark backgrounds */
+  dark: PropTypes.bool,
+};
---
+++ src/examples/HomeIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import HomeIcon from 'alys/HomeIcon';
+
+/** Basic home icon */
+export default function ExampleBasic() {
+  return <HomeIcon />;
+}
