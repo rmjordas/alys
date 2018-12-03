a2d727a6b581aee327c1f019e2436babf1dc3a73
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 19:44:45 +0800

    Add StreamIcon component

---
+++ src/components/StreamIcon/StreamIcon.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SvgIcon from '../SvgIcon';
+
+/** See <SvgIcon /> for more prop descriptions. */
+export default function StreamIcon({ dark, ...svgProps }) {
+  const commonProps = {
+    fill: 'none',
+    stroke: dark ? '#fff' : '#4C5264',
+    strokeMiterlimit: 10,
+  };
+
+  return (
+    <SvgIcon viewBox="0 0 23 16.575" {...svgProps}>
+      <g id="stream-icon" transform="translate(0 -0.025)">
+        <circle
+          {...commonProps}
+          id="ellipse"
+          cx="0.5"
+          cy="0.5"
+          r="0.5"
+          transform="translate(11 15.1)"
+        />
+        <path
+          {...commonProps}
+          id="path"
+          strokeLinecap="round"
+          d="M6.9,12.4a6.58,6.58,0,0,1,9.2,0"
+        />
+        <path
+          {...commonProps}
+          id="path-2"
+          data-name="path"
+          strokeLinecap="round"
+          d="M4.2,8.6a10.322,10.322,0,0,1,14.7,0"
+        />
+        <path
+          {...commonProps}
+          id="path-3"
+          data-name="path"
+          strokeLinecap="round"
+          d="M.5,5.1a15.512,15.512,0,0,1,22,0"
+        />
+      </g>
+    </SvgIcon>
+  );
+}
+
+StreamIcon.defaultProps = { dark: false };
+
+StreamIcon.propTypes = {
+  /** Set to `true` to make icon viewable in dark backgrounds */
+  dark: PropTypes.bool,
+};
---
+++ src/examples/StreamIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import StreamIcon from 'alys/StreamIcon';
+
+/** Basic stream icon */
+export default function ExampleBasic() {
+  return <StreamIcon />;
+}
