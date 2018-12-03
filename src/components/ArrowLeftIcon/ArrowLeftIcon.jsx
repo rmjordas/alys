85117b324f240655cc4e18ce759aca0d906f6896
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 22:32:41 +0800

    Add ArrowLeftIcon component

---
+++ src/components/ArrowLeftIcon/ArrowLeftIcon.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SvgIcon from '../SvgIcon';
+
+/** See <SvgIcon /> for more prop descriptions. */
+export default function ArrowLeftIcon({ dark, ...svgProps }) {
+  const commonProps = {
+    fill: 'none',
+    stroke: dark ? '#fff' : '#4C5264',
+    strokeLinecap: 'round',
+    strokeMiterlimit: 10,
+  };
+
+  return (
+    <SvgIcon viewBox="0 0 19 17.2" {...svgProps}>
+      <g id="arrow-left-icon" transform="translate(0 0)">
+        <line
+          {...commonProps}
+          id="line"
+          x1="17"
+          transform="translate(1.5 8.6)"
+        />
+        <path
+          {...commonProps}
+          id="path"
+          strokeLinejoin="round"
+          d="M8.6.5.5,8.6l8.1,8.1"
+        />
+      </g>
+    </SvgIcon>
+  );
+}
+
+ArrowLeftIcon.defaultProps = { dark: false };
+
+ArrowLeftIcon.propTypes = {
+  /** Set to `true` to make icon viewable in dark backgrounds */
+  dark: PropTypes.bool,
+};
---
+++ src/examples/ArrowLeftIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ArrowLeftIcon from 'alys/ArrowLeftIcon';
+
+/** Basic arrow left icon */
+export default function ExampleBasic() {
+  return <ArrowLeftIcon />;
+}
