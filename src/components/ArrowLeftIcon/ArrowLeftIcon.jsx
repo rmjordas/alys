310f4377945dc17485807048bf411f129979dfde
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 16:15:39 +0800

    Add ArrowLeftIcon component

---
+++ src/components/ArrowLeftIcon/ArrowLeftIcon.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SvgIcon from '../SvgIcon';
+
+const isDark = (dark) => (dark ? '#fff' : '#4C5264');
+
+export default function ArrowLeftIcon({ dark, ...svgProps }) {
+  return (
+    <SvgIcon {...svgProps}>
+      <g id="arrow-left" transform="translate(-512.002 -387)">
+        <path
+          id="arrow-left-2"
+          data-name="arrow-left"
+          fill="none"
+          stroke={isDark(dark)}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          d="M5.3,10.2.5,5.3,5.3.5"
+          transform="translate(512.002 387)"
+        />
+      </g>
+    </SvgIcon>
+  );
+}
+
+ArrowLeftIcon.defaultProps = { dark: false };
+
+ArrowLeftIcon.propTypes = {
+  /** Set to `true` to make icon viewable in dark backgrounds */
+  dark: PropTypes.bool,
+};
