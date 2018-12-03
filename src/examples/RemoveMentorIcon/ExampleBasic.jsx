09348e713a762766a655e135d09a8b3d5f92922a
Rodger Jordas <rmjordas@gmail.com>
2018-12-03 16:58:41 +0800

    Add RemoveMentorIcon

---
+++ src/examples/RemoveMentorIcon/ExampleBasic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import RemoveMentorIcon from 'alys/RemoveMentorIcon';
+
+/** Basic remove mentor icon */
+export default function ExampleBasic() {
+  return <RemoveMentorIcon />;
+}
