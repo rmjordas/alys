f9bb61a3e998a988e1f001318a8aca8b5986bc0c
Rodger Jordas <rmjordas@gmail.com>
2018-12-05 20:12:19 +0800

    Update Currency formatted value wrapper

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -11,7 +11,7 @@ export default function Currency({ value, locale, code, whole }) {
     maximumFractionDigits: whole ? 0 : undefined,
   });
 
-  return <span>{fmt.format(value)}</span>;
+  return <React.Fragment>{fmt.format(value)}</React.Fragment>;
 }
 
 Currency.defaultProps = {
