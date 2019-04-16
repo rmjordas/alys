eb907950e261b3442707a16f714ff615c71f443b
Rodger Jordas <rmjordas@gmail.com>
2019-04-17 01:07:18 +0800

    Register service worker in documentation app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
