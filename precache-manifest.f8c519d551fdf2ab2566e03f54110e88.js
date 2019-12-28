e447a2206afc651a6cccf92d7b67e0ca0708f193
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-28 13:40:53 +0000

    Deploy rmjordas/alys to github.com/rmjordas/alys.git:gh-pages

---
+++ precache-manifest.f8c519d551fdf2ab2566e03f54110e88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1be81823bffb3443b3a2ca5a5f16b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c88996c1c60e5657d5ea",
+    "url": "/static/css/2.eb8892a1.chunk.css"
+  },
+  {
+    "revision": "ab808a6392bd785ad48b",
+    "url": "/static/css/main.9ad597cd.chunk.css"
+  },
+  {
+    "revision": "c88996c1c60e5657d5ea",
+    "url": "/static/js/2.24639efa.chunk.js"
+  },
+  {
+    "revision": "ab808a6392bd785ad48b",
+    "url": "/static/js/main.47e2242a.chunk.js"
+  },
+  {
+    "revision": "05f106ef3e51e35b550d",
+    "url": "/static/js/runtime-main.1757f885.js"
+  },
+  {
+    "revision": "3fa9413b817c3e5fc55fb7cde4ca73ca",
+    "url": "/static/media/app.3fa9413b.svg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/github.07883e93.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad111f066c4923fa283e356d206326ce.js"
+  "/precache-manifest.f8c519d551fdf2ab2566e03f54110e88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
